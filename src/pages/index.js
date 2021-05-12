4c7ec3b0da50b1f7e40fe18a914ce73d2784f863
Victor Kist <victoraugustokist@gmail.com>
2021-05-12 19:30:32 -0300

    add FormularioCadastro.jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
-import React from "react"
+import React, { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <FormularioCadastro />
+    </>
+  );
 }
