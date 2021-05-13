a6114f92b8fa0e62ef6859f3fdb86ea2f14d192c
Victor Kist <victoraugustokist@gmail.com>
2021-05-13 06:51:55 -0300

    adicionar tailwinds

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import './src/styles/global.css';
 
 export default function Home() {
   return (
