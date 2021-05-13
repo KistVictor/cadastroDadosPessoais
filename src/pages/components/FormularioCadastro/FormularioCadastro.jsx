1fa48b430ebe625a3d7dc73ea3e1556c70dbb8a7
Victor Kist <victoraugustokist@gmail.com>
2021-05-13 20:15:58 -0300

    adicionar elementos

--- src/pages/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/pages/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,31 +1,41 @@
-import React from "react";
+import React from "react"
 import tw, { styled } from "twin.macro"
 
 const Button = tw.button`
-  bg-blue-500 hover:bg-blue-800 text-white p-2 rounded
+  bg-purple-600 hover:bg-purple-700 text-white px-2 py-1 rounded
+`
+
+const Label = tw.label`
+  text-indigo-600
+`
+
+const Input = tw.input`
+  box-content h-5 w-5 text-gray-600
 `
 
 function FormularioCadastro() {
   return (
-    <form>
-      <label>Nome</label>
-      <input type="text"/>
+    <>
+      <form>
+        <Label>Nome</Label>
+        <input type="text" />
 
-      <label>Sobrenome</label>
-      <input type="text"/>
+        <Label>Sobrenome</Label>
+        <input type="text" />
 
-      <label>CPF</label>
-      <input type="text"/>
+        <Label>CPF</Label>
+        <input type="text" />
 
-      <label>Promoções</label>
-      <input type="checkbox" />
+        <Label>Promoções</Label>
+        <input type="checkbox" checked />
 
-      <label>Novidades</label>
-      <input type="checkbox" />
+        <Label>Novidades</Label>
+        <Input type="checkbox" checked />
 
-      <Button type="submit">Activate</Button>
-    </form>
-  );
+        <Button type="submit">Cadastrar</Button>
+      </form>
+    </>
+  )
 }
 
-export default FormularioCadastro;
+export default FormularioCadastro
