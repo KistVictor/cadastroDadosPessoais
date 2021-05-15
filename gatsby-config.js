4d1bc7822be3d53d695d4d93ffa55c4f2577d878
Victor Kist <victoraugustokist@gmail.com>
2021-05-15 11:16:20 -0300

    ajustar gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,15 +6,7 @@
 
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        postCssPlugins: [
-          require("tailwindcss"),
-          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
-        ],
-      },
-    },
+    `gatsby-plugin-emotion`,
     `gatsby-plugin-gatsby-cloud`,
   ],
 }
