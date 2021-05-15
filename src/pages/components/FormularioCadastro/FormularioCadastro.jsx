dc2ad2328dc6626b6270764e7c8327bde7c8e651
Victor Kist <victoraugustokist@gmail.com>
2021-05-15 09:38:02 -0300

    centralizando formulario

--- src/pages/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/pages/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,40 +3,40 @@ import React from "react"
 function FormularioCadastro() {
   return (
     <>
-      <div className="bg-gray-50">
-        <form class="space-y-4">
+      <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <form className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <label className="text-indigo-600 block">Nome</label>
+            <input
+              type="text"
+              className="border rounded-md border-indigo-500 border-opacity-25 block"
+            />
 
-          
-          <label className="text-indigo-600 block">Nome</label>
-          <input
-            type="text"
-            className="border rounded-md border-indigo-500 border-opacity-25 block"
-          />
+            <label className="text-indigo-600 block">Sobrenome</label>
+            <input
+              type="text"
+              className="border rounded-md border-indigo-500 border-opacity-25 block"
+            />
 
-          <label className="text-indigo-600 block">Sobrenome</label>
-          <input
-            type="text"
-            className="border rounded-md border-indigo-500 border-opacity-25 block"
-          />
+            <label className="text-indigo-600 block">CPF</label>
+            <input
+              type="text"
+              className="border rounded-md border-indigo-500 border-opacity-25 block"
+            />
 
-          <label className="text-indigo-600 block">CPF</label>
-          <input
-            type="text"
-            className="border rounded-md border-indigo-500 border-opacity-25 block"
-          />
+            <label className="text-indigo-600">Promoções</label>
+            <input type="checkbox" className="mx-3" />
 
-          <label className="text-indigo-600">Promoções</label>
-          <input type="checkbox" className="mx-3" />
+            <label className="text-indigo-600">Novidades</label>
+            <input type="checkbox" className="mx-3" />
 
-          <label className="text-indigo-600">Novidades</label>
-          <input type="checkbox" className="mx-3" />
-
-          <button
-            className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-            type="submit"
-          >
-            Cadastrar
-          </button>
+            <button
+              className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+              type="submit"
+            >
+              Cadastrar
+            </button>
+          </div>
         </form>
       </div>
     </>
